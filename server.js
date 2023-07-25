7a4f2a2e88bae772e6b89623c295f897dc8bb46d
doraemon0807 <98371903+doraemon0807@users.noreply.github.com>
2023-07-25 00:23:15 -0400

    graphql queries

--- server.js
+++ server.js
@@ -1,24 +1,144 @@
 import { ApolloServer, gql } from "apollo-server";
 
+let tweets = [
+  {
+    id: "1",
+    text: "first",
+    userId: "2",
+  },
+  {
+    id: "2",
+    text: "second",
+    userId: "1",
+  },
+];
+
+let users = [
+  {
+    id: "1",
+    firstName: "Yun",
+    lastName: "Hong",
+  },
+  {
+    id: "2",
+    firstName: "Elon",
+    lastName: "Mask",
+  },
+];
+
 const typeDefs = gql`
-  type Query {
-    allTweets: [Tweet]
-    tweet(id: ID): Tweet
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    """
+    Is the sum of firstName + lastName
+    """
+    fullName: String!
   }
 
   type Tweet {
-    id: ID
-    text: String
+    id: ID!
+    text: String!
     author: User
   }
 
-  type User {
-    id: ID
-    username: String
+  type Query {
+    allMovies: [Movie!]!
+    allTweets: [Tweet!]!
+    allUsers: [User!]!
+    tweet(id: ID!): Tweet
+    movie(id: String!): Movie
+  }
+
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
+  }
+
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    allUsers() {
+      return users;
+    },
+    allMovies() {
+      return fetch("https://yts.mx/api/v2/list_movies.json")
+        .then((res) => res.json())
+        .then((json) => json.data.movies);
+    },
+    tweet(_, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+    movie(_, { id }) {
+      return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        .then((res) => res.json())
+        .then((json) => json.data.movie);
+    },
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const userExist = users.find((user) => user.id === userId);
+
+      if (!userExist) {
+        throw new Error("User doesn't exist");
+      }
+
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+        userId,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter((tweet) => tweet.id !== id);
+      return true;
+    },
+  },
+  User: {
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
+    },
+  },
+  Tweet: {
+    author({ userId }) {
+      return users.find((user) => user.id === userId);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
